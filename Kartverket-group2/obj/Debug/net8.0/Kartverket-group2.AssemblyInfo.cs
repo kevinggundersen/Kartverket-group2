507a42f2a6a919e0e362751353027112d0ab69ea
kevinggundersen <159529468+kevinggundersen@users.noreply.github.com>
2024-09-29 19:54:58 +0200

    Added pages

--- Kartverket-group2/obj/Debug/net8.0/Kartverket-group2.AssemblyInfo.cs
+++ Kartverket-group2/obj/Debug/net8.0/Kartverket-group2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kartverket-group2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77006fe39e6350c96f7e5e2d6d365e35b6cb66c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e3a681cf0caea78f41718420b0c4fd3971b20bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kartverket-group2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kartverket-group2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
