b67234801c5cb46925e4488e97278d21a913e5bd
kevinggundersen <159529468+kevinggundersen@users.noreply.github.com>
2024-09-30 10:16:35 +0200

    Reverted last commit. Hash fac0a250f2f383bfd2985d334e8386c097b10018

--- Kartverket-group2/Controllers/MapController.cs
+++ Kartverket-group2/Controllers/MapController.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;  // Add this line
-using System.Collections.Generic;
-using Kartverket_group2.Models;  // Your model namespace
 
 namespace Kartverket_group2.Controllers
 {
@@ -10,28 +7,6 @@ namespace Kartverket_group2.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        [HttpPost]
-        public ActionResult SaveShapes(string shapeData)
-        {
-            // Deserialize the JSON data to a list of Shape objects
-            List<Shape> shapes = JsonConvert.DeserializeObject<List<Shape>>(shapeData);
-
-            // Store the shape data in TempData for now (or in a database in the future)
-            TempData["ShapeData"] = shapeData;
-
-            return RedirectToAction("ViewShapes");
-        }
-
-        public ActionResult ViewShapes()
-        {
-            // Get the shape data from TempData
-            var shapeData = TempData["ShapeData"] as string;
-            List<Shape> shapes = JsonConvert.DeserializeObject<List<Shape>>(shapeData);
-
-            // Pass the shapes list to the view
-            return View(shapes);
         }
     }
 }
