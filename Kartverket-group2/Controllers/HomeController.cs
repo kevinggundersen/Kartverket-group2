273974e69cb746645e62d11d51147887aa63ba91
Natasha Engemann <Natashaengemann@outlook.com>
2024-11-06 12:10:25 +0100

    added profile page. moved register and login to css

--- Kartverket-group2/Controllers/HomeController.cs
+++ Kartverket-group2/Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace Kartverket_group2.Controllers
         {
             return View();
         }
+        public IActionResult Profile()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
